a5407940e5ed8f90c5f84c420bd06d948a4f8069
Sebastián Rojo <sebastianrojocollao@gmail.com>
2026-02-04 13:02:37 -0300

    Agregar archivos de configuración y estructura inicial del proyecto para la API de reseñas de películas

---
+++ src/main/java/cl/sebastianrojo/moviereview/MovieReviewApiApplication.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.moviereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sebastianrojo/moviereview/MovieReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.moviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
